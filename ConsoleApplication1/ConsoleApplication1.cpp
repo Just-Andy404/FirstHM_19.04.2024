95b84452d86137d633429101c0547345d6a7ded7
Andy <andyivanov2007@gmail.com>
2024-07-16 21:16:32 +0300

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,100 @@
+﻿/*
+Создайте шаблонный класс обычной очереди для работы с
+с целыми значениями. Требуется создать реализации для
+типичных операций над элементами:
+■ IsEmpty - проверка очереди на пустоту;
+■ IsFull - проверка очереди на заполнение;
+■ Enqueue - добавление элемента в очередь;
+■ Dequeue - удаление элемента из очереди;
+■ Show - отображение всех элементов очереди на экран.
+*/
+#include <iostream>
+#include "Queue.h"
+
+void showMenu() {
+    cout << "Меню:\n";
+    cout << "1. Проверка очереди на пустоту\n";
+    cout << "2. Проверка очереди на заполнение\n";
+    cout << "3. Добавление элемента в очередь\n";
+    cout << "4. Удаление элемента из очереди\n";
+    cout << "5. Отображение всех элементов очереди\n";
+    cout << "6. Выход\n";
+    cout << "Выберите пункт меню: ";
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int capacity;
+    cout << "Введите максимальную емкость очереди: ";
+    cin >> capacity;
+
+    Queue<int> queue(capacity);
+
+    int choice;
+    do {
+        showMenu();
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            if (queue.IsEmpty()) {
+                cout << "Очередь пуста.\n";
+            }
+            else {
+                cout << "Очередь не пуста.\n";
+            }
+            break;
+
+        case 2:
+            if (queue.IsFull()) {
+                cout << "Очередь заполнена.\n";
+            }
+            else {
+                cout << "Очередь не заполнена.\n";
+            }
+            break;
+
+        case 3:
+            if (!queue.IsFull()) {
+                int num;
+                cout << "Введите число для добавления в очередь: ";
+                cin >> num;
+                queue.Enqueue(num);
+                cout << "Элемент добавлен.\n";
+            }
+            else {
+                cout << "Очередь заполнена, невозможно добавить элемент.\n";
+            }
+            break;
+
+        case 4:
+            if (!queue.IsEmpty()) {
+                int dequeued = queue.Dequeue();
+                if (dequeued != -1) {
+                    cout << "Удален элемент: " << dequeued << endl;
+                }
+            }
+            else {
+                cout << "Очередь пуста, невозможно удалить элемент.\n";
+            }
+            break;
+
+        case 5:
+            cout << "Элементы очереди: ";
+            queue.show();
+            break;
+
+        case 6:
+            cout << "Выход из программы.\n";
+            break;
+
+        default:
+            cout << "Неверный выбор, попробуйте снова.\n";
+            break;
+        }
+    } while (choice != 6);
+
+    return 0;
+}
+
---
+++ ConsoleApplication1/Queue.h
@@ -0,0 +1,97 @@
+﻿/*
+Создайте шаблонный класс обычной очереди для работы с
+с целыми значениями. Требуется создать реализации для
+типичных операций над элементами:
+■ IsEmpty - проверка очереди на пустоту;
+■ IsFull - проверка очереди на заполнение;
+■ Enqueue - добавление элемента в очередь;
+■ Dequeue - удаление элемента из очереди;
+■ Show - отображение всех элементов очереди на экран.
+*/
+
+#pragma once
+#include <iostream>
+using namespace std;
+
+template <class Q>
+
+class Queue
+{
+	Q* _queue;
+	int _capacity;
+	int _size;
+public:
+	Queue();
+	Queue(int);
+	~Queue();
+	bool IsEmpty();
+	bool IsFull();
+	void Enqueue(Q num);
+	Q Dequeue();
+	void show();
+};
+
+template<class Q>
+inline Queue<Q>::Queue() : Queue(1) {}
+
+template<class Q>
+inline Queue<Q>::Queue(int capacity) : _size(0)
+{
+	_capacity = capacity;
+	_queue = new Q[_capacity];
+}
+
+template<class Q>
+inline Queue<Q>::~Queue()
+{
+	delete[] _queue;
+}
+
+template<class Q>
+inline bool Queue<Q>::IsEmpty()
+{
+	return _size == 0;
+}
+
+template<class Q>
+inline bool Queue<Q>::IsFull()
+{
+	return _size == _capacity;
+}
+
+template<class Q>
+inline void Queue<Q>::Enqueue(Q num)
+{
+	if (!IsFull())
+	{
+		_queue[_size++] = num;
+	}
+}
+
+template<class Q>
+inline Q Queue<Q>::Dequeue()
+{
+	if (!IsEmpty())
+	{
+		int temp = _queue[0];
+		for (int i = 1; i < _size - 1; i++)
+		{
+			_queue[i] = _queue[i + 1];
+		}
+		_size--;
+		return temp;
+	}
+	else
+		return -1;
+	return Q();
+}
+
+template<class Q>
+inline void Queue<Q>::show()
+{
+	cout << "\n-------------------------------------\n";
+	for (int i = 0; i < _size; i++) {
+		cout << _queue[i] << " ";
+	}
+	cout << "\n-------------------------------------\n";
+}
